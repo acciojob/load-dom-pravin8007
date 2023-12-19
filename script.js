7011c4b07955a942869222f43faea84fe8584840
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-19 10:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+            // DOM is loaded, let's add the string
+            var body = document.body;
+            
+            // Make sure the body is empty
+            body.innerHTML = '';
+
+            // Create a new paragraph element
+            var paragraph = document.createElement('p');
+            
+            // Set the text content of the paragraph
+            paragraph.textContent = "DOM load success";
+
+            // Append the paragraph to the body
+            body.appendChild(paragraph);
+	     });
