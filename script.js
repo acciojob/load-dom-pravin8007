f8e28cd0a26e359dfa11baf1d47250152d8a23e0
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-19 11:11:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,23 +5,14 @@ document.addEventListener('DOMContentLoaded', function () {
             
             // Make sure the body is empty
             body.textContent = '';
+	body.textContent = "DOM load success"
 
             // Create a new paragraph element
-            var paragraph = document.createElement('p');
+            // var paragraph = document.createElement('p');
             
-            // Set the text content of the paragraph
-            paragraph.textContent = 'DOM load success';
+            // // Set the text content of the paragraph
+            // paragraph.textContent = 'DOM load success';
 
-            // Append the paragraph to the body
-            body.appendChild(paragraph);
+            // // Append the paragraph to the body
+            // body.appendChild(paragraph);
 	     });
-describe('DOM content', () => {
-  it('Loads DOM content successfully', () => {
-    cy.visit(baseUrl);
-    
-    // Use should with a custom assertion to wait for the content to be present
-    cy.get('body').should(($body) => {
-      expect($body.text()).to.include('DOM load success');
-    });
-  });
-});
